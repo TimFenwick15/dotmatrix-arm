e42ad120f85e816a1fdfae916f60de5c46f1aa90
TimFenwick15 <tjf20@hotmail.co.uk>
2020-05-02 14:19:31 +0100

    Added shell script for failing test to try on Travis

---
+++ RunTests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+gcc -DDEBUG -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F411xE -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -DTEST -I"include" -I"system/include" -I"system/include/cmsis" -I"system/include/stm32f4-hal" src/Sprite.c Unity/*.c -o Unity/test.exe
+
+./Unity/test.exe
